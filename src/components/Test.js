968e5844846f80807c1312a07abbf235a07a57e6
Brian Harvey <brian.harv3y@gmail.com>
2021-01-08 15:39:54 -0800

    retrieve data from firebase

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import firebase from '../firebase';
 
 const Test = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [testData, setTestData] =  useState([]);
+
+  const ref = firebase.firestore().collection('test');
+
+  const getTestData = () => {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach(doc => {
+        items.push(doc.data());
+      });
+      setTestData(items)
+      setLoading(false)
+    })
+  }
+
+  useEffect(()=> {
+    getTestData();
+  },[]);
+
+  if (loading) {
+    return <h3>loading...</h3>
+  }
+
   return(
     <React.Fragment>
-      <h3> This is test data</h3>
+      <h3> Person:</h3>
+      {testData.map((test) => (
+        <div key={test.id}>
+        <p>{test.name}, {test.age}</p>
+        <p>{test.desc}</p> 
+        </div>
+      )
+      )}
       <hr/>
     </React.Fragment>
   )
